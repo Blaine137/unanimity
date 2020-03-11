d50b131028ab27e77833a82ea8a271a619780c25
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-03-11 14:39:36 -0400

    started messenger authentication and axios auth
    was unable to complete messenger atuthentication becouse axios now requires authentication. Started working on axios authentication but have not figured it out.

--- unanimity/src/axios.js
+++ unanimity/src/axios.js
@@ -1,7 +1,23 @@
 import Axios from 'axios';
+const token = {
 
+    user: "iKyUinRauGTi1n9roPQ4NFFo79Z2",
+    password: "Un@n1m1tyDB"
+
+};
 const instance = Axios.create ( {
     baseURL: 'https://unanimity-c1aa9.firebaseio.com/'
+   
 } );
+console.log(Axios.defaults.headers.common)
+Axios.defaults.headers.common['Authorization'] = token;
+// Add a request interceptor
+/*Axios.interceptors.request.use(function (config) {
+
+    config.headers.Authorization =  token;
+
+    return config;
+
+});*/
 
 export default instance;
