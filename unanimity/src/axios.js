460597037564e13259455d17a59554e0bdaa5379
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-03-11 23:18:13 -0400

    Completed Auth
    Completed the functionality of auth and did the styles for css. Did not do mobile first design as we decided to just work on desktop at the moment.

--- unanimity/src/axios.js
+++ unanimity/src/axios.js
@@ -1,22 +1,9 @@
 import Axios from 'axios';
- const token = {
-     "uid": "ee8b580c09f3-408a-b054"
-     
- };
+
 const instance = Axios.create ( {
-    baseURL: 'https://unanimity-c1aa9.firebaseio.com/'
+    baseURL: 'https://unanimity-8d56c.firebaseio.com/'
    
 } );
-// console.log('before: ', instance.defaults.headers.common)
-// instance.defaults.headers.common['auth'] = token; 
-// console.log('after: ', instance.defaults.headers.common['auth'].uid)
-// Add a request interceptor
-/*Axios.interceptors.request.use(function (config) {
 
-    config.headers.Authorization =  token;
-
-    return config;
-
-});*/
 
 export default instance;
