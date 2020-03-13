321dafeaefa03552bc15738c31c4c41dacb5a3b4
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-03-13 19:59:45 -0400

    restesr new user
    Completed the ability to add a new user. it adds the user in all corsponding tables. it also contains validation to make sure the user dose not allreadt exzist. PS. add chatroom button
          new users will not have a usersChatRooms/ucr(userID)/chatRooms[array]
          if we use any axios get usersChatRooms/ucr(userID)/chatRooms[array] we need to handle the error if it dosent exzist.                                                      could not initiate chatRooms arry to an empty array in firebase

--- unanimity/src/components/Sidebar/Sidebar.js
+++ unanimity/src/components/Sidebar/Sidebar.js
@@ -21,76 +21,92 @@ class Sidebar extends Component {
 
                     chatRoomIDs = this.props.usersChatRoomsID; // All the chat rooms that the current authenticated user is in.
                     //console.log(chatRoomIDs)
-                        //for each chatRoomID as singleChatRoomID
+                    //for each chatRoomID as singleChatRoomID
                     chatRoomIDs.forEach( ( singleChatRoomID ) => {
                                                          
-                            //get alls usersID that are in the singleChatRoomID
+                        //get alls usersID that are in the singleChatRoomID
                         axios.get( 'chatRoomsUsers/cru' + singleChatRoomID + '.json' ).then( ( res ) => {
                             
                             let chatRoomUsersArray = Object.entries( res.data ); //converts the data into an array
                            
-                                //[1][1] navigates to userID in the array. for each userID as chatRoomUserID
-                              
+                                //[1][1] navigates to userID in the array.
+                                // for each userID as chatRoomUserID
                                 chatRoomUsersArray[ 1 ][ 1 ].forEach( ( chatRoomUserID ) => {
                                
-                                    //if current chatRoomuserID == current userID logged in                                                                                                                                 
-                                        //console.log(chatRoomUserID);
-                                        //gets the data for that current chatRoomUserID
-                                        if( chatRoomUserID !== this.props.userID ) {
-                                     axios.get( 'users/u' + chatRoomUserID + '/userName.json' ).then(
-                                        ( e ) => {
-                                                    //this if prevents a infinite loop.
-                                            if( this.state.chatRoomName !== e.data ){
+                                                                                                                                                                   
+                                    //console.log(chatRoomUserID);
+                                    //gets the data for that current chatRoomUserID
+                                    //if current chatRoomuserID === current userID logged in  
+                                    if( chatRoomUserID !== this.props.userID ) {
 
-                                                //remove our userId chatRoomUserID
+                                        //axios get username for the current chatRoom user
+                                        axios.get( 'users/u' + chatRoomUserID + '/userName.json' ).then(
+                                            ( e ) => {
 
-                                                this.setState( { chatRoomName: e.data} );
-                                                
-                                            } 
-                                                                                                                                                                              
-                                        }
-                            
-                                    );
+                                                //this if prevents a infinite loop.
+                                                if( this.state.chatRoomName !== e.data ){
 
-                                }
+                                                    //set chatRoomName to username of the recipent in the chatroom
+                                                    this.setState( { chatRoomName: e.data} );
+                                                    
+                                                }//prevent infinateloop if
+                                                                                                                                                                                
+                                        });//axios get username for the current chatRoom user
+
+                                    }//if current chatRoomuserID === current userID logged in 
                                                                                                     
-                            });
+                                });// for each userID as chatRoomUserID
                     
-                        } ).catch( ( error ) => { console.log( error ) } );
+                        } ).catch( 
+                            ( error ) => {
+
+                                console.log( error ) 
+
+                            } 
+                        );//get alls usersID that are in the singleChatRoomID
                         i++;
-                        sidebarDisplay.push( ( <div onClick={ ( ) => { this.props.setCurrentChatRoomID( singleChatRoomID ) } } key = { i}  className = { styles.users } >
+                        sidebarDisplay.push( ( <div onClick={ ( ) => { this.props.setCurrentChatRoomID( singleChatRoomID ) } } key = { i }  className = { styles.users } >
                                                         <h3>{ this.state.chatRoomName }</h3>
                                         </div>)); 
 
-                    });                  
+                    }); //for each chatRoomID as singleChatRoomID                
         }
 
         /* check is menu button is clicked, show or hide the sidebar */
-        if(this.props.showSidebar){
+        //if sidebar is open
+        if( this.props.showSidebar ) {
+            
+            //set css to show the sidebar
+            sidebar = <div className = { styles.sidebarContainer } style = { {transform: 'translateX(0%)'} } >
 
-        sidebar = <div className = { styles.sidebarContainer } style = { {transform: 'translateX(0%)'} }>
+                        { sidebarDisplay }
 
-                { sidebarDisplay }
+                    </div>;
 
-            </div>;
+        } 
+        //this.props.showSideBar is false and user has closed the sidebar
+        else {
 
-        }else{
+            //set the css to hide the sidebar by moving it left 100%
+            sidebar = <div className = { styles.sidebarContainer } style = { { transform: 'translateX(-100%)' } } >
 
-            sidebar = <div className = { styles.sidebarContainer } style = { { transform: 'translateX(-100%)' } }>
+                        { sidebarDisplay }
 
-            { sidebarDisplay }
-
-            </div>;
+                    </div>;
 
         }
 
         return(
-            <div>
-                { sidebar }
-            </div>
-        );
 
-    }
+            <div>
+
+                { sidebar }
+
+            </div>
+
+        );//return()
+
+    }//render()
     
 }
 export default Sidebar;
