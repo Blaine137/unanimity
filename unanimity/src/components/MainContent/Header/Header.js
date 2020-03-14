584d4544f199230ce7f50be3e02f719f3219e645
Blaine137 <52352305+Blaine137@users.noreply.github.com>
2020-03-13 21:02:15 -0400

    animated the burger menu

--- unanimity/src/components/MainContent/Header/Header.js
+++ unanimity/src/components/MainContent/Header/Header.js
@@ -1,22 +1,50 @@
 import React from 'react';
 import styles from './Header.module.css';
 
-const header = (props) => {
-  
-    return(
-        <header className={styles.header}>
-            
-            <div onClick={ ( ) => { props.showSidebar() } } className={styles.burger}  >
+const header = ( props ) => {
+ 
+            let burger = null;
 
-                        <div className={styles.burgerTop}></div>
-                        <div className={styles.burgerBottom}></div>
-                        
-            </div>
+                    //if props.showSidebar is true
+            if( props.showSidebar ) {
 
-            <h3 className={styles.header3}>
-                {props.currentChatRoomName}
-            </h3>
-        </header>
-    );
+                    //make the burger button a X
+                burger = <div onClick = { ( ) => { props.toggleSidebar( ) } } className = { styles.close }  >
+
+                                <div className = { styles.closeTop } ></div>
+                                <div className = { styles.closeMiddle } ></div>
+                                <div className = { styles.closeBottom } ></div>
+                                
+                    </div>;
+
+            } else {
+                        //if false then make the burger button 
+                burger = <div onClick={ ( ) => { props.toggleSidebar( ) } } className = { styles.burger }  >
+
+                            <div className = { styles.burgerTop } ></div>
+                            <div className = { styles.burgerMiddle } ></div>
+                            <div className = { styles.burgerBottom } ></div>
+                
+                        </div>;
+
+            }
+    
+        return(
+
+            <header className = { styles.header } >
+                
+                { burger }
+
+                <h3 className = { styles.header3 } >
+
+                    { props.currentChatRoomName } 
+
+                </h3>
+
+            </header>
+
+        );//return
+
 }
+
 export default header;
