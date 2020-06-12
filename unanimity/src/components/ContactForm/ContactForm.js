126599994f34d9bc267bcd2d2bfcac0d6636756c
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-06-12 18:56:59 -0400

    css styles
    styled message component to be easier to read. Also styled some in chatroom for  readability. did  some code cleanup in contact from.

--- unanimity/src/components/ContactForm/ContactForm.js
+++ unanimity/src/components/ContactForm/ContactForm.js
@@ -1,50 +1,48 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from 'react';
 import styles from './ContactForm.module.scss';
 
-class ContactForm extends Component{
+class ContactForm extends Component {
 
-    render(){
+    render( ) {
 
             return(
 
                 <Fragment>
-                                <div className = { styles.wrapper }>
 
-                            <img src="../../../unanimity-large-logo.svg" alt="Unanimity Messenger Logo. Harmony through words."/>
+                    <div className = { styles.wrapper }>
+
+                        <img src="../../../unanimity-large-logo.svg" alt="Unanimity Messenger Logo. Harmony through words."/>
                             
-                            <form className = { styles.form } action="/" > {/* removed the name from the email input to get rid of querystring since post method would not work. */}
-                                <fieldset>
+                        <form className = { styles.form } action="/" > { /* removed the name from the email input to get rid of querystring since post method would not work. */ }
 
-                                    <legend>Contact Unanimity!</legend>
+                            <fieldset>
+
+                                <legend>Contact Unanimity!</legend>
                                     
-                                    <label htmlFor = "Email" >Email</label>
-                                    <input aria-label = "Email Text input" type = "Email" id = "Email"  className = { styles.input } /> 
+                                <label htmlFor = "Email" >Email</label>
+                                <input aria-label = "Email Text input" type = "Email" id = "Email"  className = { styles.input } /> 
 
-                                    <label htmlFor = "Comments">Comments</label>
-                                    <textarea id = "Comments" rows="4" cols="40" placeholder = "Enter your comment here..."></textarea>
+                                <label htmlFor = "Comments">Comments</label>
+                                <textarea aria-label = "Enter your comments here." id = "Comments" rows="4" cols="40" placeholder = "Enter your comment here..."></textarea>
 
-                                    <input aria-label = "Submit Login information" type = "submit" value = "Submit" className = { styles.submit } onClick={ () => {
+                                <input aria-label = "Submit contact form button" type = "submit" value = "Submit" className = { styles.submit } onClick={ ( ) => {
                                         
-
                                         alert('Thank you for contacting Unanimity, your form will be reviewed within 24 hours!');
 
-                                    } }/> 
+                                } } /> 
 
-                                </fieldset>
+                            </fieldset>
                                 
-                            </form>
+                        </form>
                             
-                        </div>
+                    </div>
+
                 </Fragment>
 
-            );
+            );//return
 
-    }
+    };//render
 
-} //end of class
-
-    
-
-
+}; //end of class
 
 export default ContactForm;
