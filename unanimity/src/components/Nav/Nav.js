81a105a1bff4b4fd7490d25e702b0142517844f0
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-09-28 18:59:47 -0400

    react router
    App .js now uses browser router and react router switch to handle moving from page to page. The navigation and all links have been updated to use react router.

--- unanimity/src/components/Nav/Nav.js
+++ unanimity/src/components/Nav/Nav.js
@@ -1,31 +1,17 @@
 import React from 'react';
 import './Nav.module.scss';
+import { Link } from 'react-router-dom';
 
 const nav = ( props ) => {
-
     return(
-
         <nav>
-
-            <ul>
-                
-                <li onClick={ () => {
-                    props.goToAuth() //localhost shows error coming from this line of code ??????
-                } }>
-                    Login/Register
-                </li>
-                <li onClick = { () => {
-                    props.goToContact()
-                } }> 
-                    Contact
-                </li>
-
+            <ul>          
+                <li><Link style={{color: '#05386B'}} exact="true" to="/" >Home</Link></li>
+                <li><Link style={{color: '#05386B'}} to="/contact" >Contact</Link></li>
+                <li><Link style={{color: '#05386B'}} to="/login" >Login</Link></li>
             </ul>  
-
         </nav>
-
     );
-
 };
 
 export default nav;
