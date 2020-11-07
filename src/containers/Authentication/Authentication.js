48b03f94fbcccf80d7698670f3509938e22ebf4d
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-11-07 18:18:34 -0500

    Update Authentication.js
    refractored some .then promises code to async await

--- src/containers/Authentication/Authentication.js
+++ src/containers/Authentication/Authentication.js
@@ -35,7 +35,7 @@ let Authentication = props => {
     }, []);
 
     const checkForNewUser = async (event, newUser, newPassword) => {    
-        event.preventDefault(); //prevent reload of page due to form submission 
+        event.preventDefault();
         let newUserValue = newUser.value;
         let newPasswordValue = newPassword.value;
         let newUserID = null;
@@ -47,12 +47,10 @@ let Authentication = props => {
         newUserID = DOMPurify.sanitize(newUserID);
         newUserID = newUserID.replace(/[^\w]/g,'');
         
-        //a valid length
+        //username a valid length
         if(newUserValue.length > 10 || newPasswordValue.length > 20) {
             props.setNotification([<Alert alertMessage="Username must be less than 10 characters and password must be less than 20." alertClose={ closeNotification }/>]);
-         }//if valid length
-         //if newUser and newPassword not null
-         else if(!newUserValue || !newPasswordValue || newUserValue < 5 || newPasswordValue < 5) {
+         } else if(!newUserValue || !newPasswordValue || newUserValue < 5 || newPasswordValue < 5) {
             props.setNotification([<Alert alertMessage="Username and password must be 5 characters long and only contain alphabetical and numerical values." alertClose={ closeNotification }/>])
          } else {
             let getUserId = async () => {
@@ -62,7 +60,8 @@ let Authentication = props => {
                     //try to get the username they are wanting to register as
                     let resName = await axios.get('userIDByUsername/' + newUserValue + '.json');
                     if(!resName.data) {
-                        setNewUser(newUserValue, newPasswordValue, newUserID); //create user if the input does not exist
+                        //create user if the username is not taken
+                        setNewUser(newUserValue, newPasswordValue, newUserID); 
                     } else {
                         props.setNotification([<Alert alertMessage="Username is already taken!" alertClose={ closeNotification }/>]);
                     }
@@ -75,59 +74,60 @@ let Authentication = props => {
     }
 
     //sets users in db
-    const setNewUser = (newUser, newPassword, newUserID) => {
+    const setNewUser = async (newUser, newPassword, newUserID) => {
         newUser = DOMPurify.sanitize(newUser);
         newUser = newUser.replace(/[^\w]/g,'');
         newPassword = DOMPurify.sanitize(newPassword);
         newPassword = newPassword.replace(/[^\w^!?$]/g,'');
         newUserID = DOMPurify.sanitize( newUserID ); 
         newUserID = newUserID.replace(/[^\w!?$]/g,'');      
-        //add user to Users in db
         let newCompleteUser = {
-                //passwordHash is a npm install. generates by default has 8 salts and strong one way encryption.
-                password: passwordHash.generate(newPassword),
-                userID: newUserID,
-                userName: newUser
+            password: passwordHash.generate(newPassword),
+            userID: newUserID,
+            userName: newUser
         };
         //sets new users in users
-        axios.put('users/u' + newUserID + '.json' , newCompleteUser);   
-        // adds user to userIDByUsername
-            //create object
-            let userIDByUsername = {};
-            //set property of object name to newUser and then set the value of the property to newUserID
-            userIDByUsername[newUser] = newUserID;
-            //axios get old usernames and ID
-            axios.get('userIDByUsername.json').then(
-                e => {
-                    //set old data(not including new user)
-                    let oldUserIDByUsername = e.data;
-                    //combine new user and old users
-                    let combinedUserIDByUsername = { ...oldUserIDByUsername, ...userIDByUsername };
-                    //convert string of int to actually integer so incrementing works
-                    let updatedNextUserID = parseInt(newUserID);
-                    updatedNextUserID++;
-                    //sets nextUserId to correct ID
-                    combinedUserIDByUsername.nextUserID = updatedNextUserID;
-                    //update db to latest version
-                    axios.put('userIDByUsername.json', combinedUserIDByUsername);
-                }
-            );
-        // end of adds user to userIDByUsername
-        //start add to usersChatRooms in DB
-                //ucr stands for UserChatRoom
-                let newUCR = {
-                    chatRooms: [],
-                    userID: newUserID
-                }
-                axios.put('usersChatRooms/ucr' + newUserID + '.json', newUCR);
-        //end of add to usersChatRooms in DB
+        axios.put('users/u' + newUserID + '.json' , newCompleteUser);  
+
+        //-----adds user to userIDByUsername-----
+
+        let userIDByUsername = {};
+        //set property of object name to newUser and then set the value of the property to newUserID
+        userIDByUsername[newUser] = newUserID;
+        //axios get old usernames and ID
+        try {
+            let oldUserIDByUsername = await axios.get('userIDByUsername.json');
+            oldUserIDByUsername = oldUserIDByUsername.data
+            let combinedUserIDByUsername = { ...oldUserIDByUsername, ...userIDByUsername };
+            let updatedNextUserID = parseInt(newUserID);
+            updatedNextUserID++;
+            //sets nextUserId to correct ID
+            combinedUserIDByUsername.nextUserID = updatedNextUserID;
+            //update db to latest version
+            axios.put('userIDByUsername.json', combinedUserIDByUsername);
+        } catch(error) {
+            return 300;
+        }
+        //-----end of adds user to userIDByUsername-----
+
+        //-----start add to usersChatRooms in DB-----
+
+            //ucr stands for UserChatRoom
+            let newUCR = {
+                chatRooms: [],
+                userID: newUserID
+            }
+            axios.put('usersChatRooms/ucr' + newUserID + '.json', newUCR);
+
+        //-----end of add to usersChatRooms in DB-----
+
         //inform user that account was created
         let accountMessage = DOMPurify.sanitize("Your account has been created! Username: '" + newUser + "'");
         accountMessage = accountMessage.replace(/[^\w\s!?$]/g,'');
         props.setNotification([<Alert alertMessage={ accountMessage } alertClose={ closeNotification }/>]);
     }
     
-    const checkName = (authValues, userNameElement, passwordElement) => {
+    const checkName = async (authValues, userNameElement, passwordElement) => {
         let username = userNameElement.value || userNameElement;
         let password = passwordElement.value || passwordElement;
         let userID = null;
@@ -135,69 +135,58 @@ let Authentication = props => {
         username = username.replace(/[^\w]/g,'');
         password = DOMPurify.sanitize(password);
         password = password.replace(/[^\w^!?$]/g,'');
-        userID = DOMPurify.sanitize(userID);
-        userID = userID.replace(/[^\w^!?$]/g,'');
-        //prevents page from reloading. forms by default cause pages to reload.
+
         if(authValues) { authValues.preventDefault(); }
-        //if username was provided
         if(username) {
             //make usernames non-caseSensitive
             username = username.toLowerCase();
             //get userId by username 
-            axios.get('userIDByUsername/' + username + '.json').then(e => {
-                //if username was not found
-                if(!e.data) {
+            try {
+                userID = await axios.get('userIDByUsername/' + username + '.json');
+                userID = userID.data;
+                if(!userID) {
                     props.setNotification([<Alert alertMessage="Incorrect username or password." alertClose={ closeNotification }/>]);
                 } else {
-                    //set the userID if the username was found
-                    userID = e.data;
                     //now that we know the username is exist and we have the userID for that username check the password
-                    //if we have a password
                     if(password) { checkPwdForUserID(username, userID, password); }
                 }
-            })
+            } catch(error) {
+                return 300;
+            }            
         }
     }
 
-    const checkPwdForUserID = (checkUsername, checkUserID, checkPassword) => {
+    const checkPwdForUserID = async (checkUsername, checkUserID, checkPassword) => {
        checkUsername = DOMPurify.sanitize(checkUsername);
        checkUsername = checkUsername.replace(/[^\w]/g,'');
        checkUserID = DOMPurify.sanitize(checkUserID);
        checkUserID = checkUserID.replace(/[^\w]/g,'');
        checkPassword = DOMPurify.sanitize(checkPassword);
        checkPassword = checkPassword.replace(/[^\w^!?$]/g,'');
-        //axios get password for a given user
-        axios.get('users/u' + checkUserID + '/password.json').then(
-            e => {
-                //if pwd is correct
-                // e.data is the 
-                if(passwordHash.verify(checkPassword, e.data)) {    
-                    props.setUserId(checkUserID);
-                    props.setUsername(checkUsername); 
-                    setTimeout(() => { props.setAuthentication(true); } ,200)                           
-                }
-                //pwd was incorrect
-                else {
-                    //pwd was wrong so set authenticed to false to make sure it failed. and set username and userID to null
-                    props.setUserId(null);
-                    props.setUsername(null);
-                    props.setAuthentication(false);
-                    //tell the user that credientals were incorrect
-                    props.setNotification([<Alert alertMessage="Incorrect username or password." alertClose={ closeNotification }/>]);
-                }
+        try {
+            let hashedPassword = await axios.get('users/u' + checkUserID + '/password.json');
+            if(passwordHash.verify(checkPassword, hashedPassword)) {    
+                props.setUserId(checkUserID);
+                props.setUsername(checkUsername); 
+                setTimeout(() => { props.setAuthentication(true); } ,200)                           
+            } else {
+                //pwd was wrong so set authenticated to false to make sure it failed. and set username and userID to null
+                props.setUserId(null);
+                props.setUsername(null);
+                props.setAuthentication(false);
+                props.setNotification([<Alert alertMessage="Incorrect username or password." alertClose={ closeNotification }/>]);
             }
-        )
+        } catch {
+            return 300;
+        }  
     }
 
     const closeNotification = () => props.setNotification(null);
     
     const ifAuthenticated = () => {
-        //if authenticated go to messenger
         if(props.authenticated) {
             messenger =  <Messenger/>;
-        } 
-        //else not authenticated stay on login page to login in
-        else {
+        } else {
             messenger = <main><Nav/><LoginForm checkName={ checkName } checkForNewUser={ checkForNewUser}/></main>;              
         }
     }
