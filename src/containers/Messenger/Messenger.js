e5c95d9a10a5af278e9881383d63e0496d1e48f6
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-11-30 11:34:44 -0500

    Fixed logging in and out bug
    After logging out then logging back in the chatrooms would not show up in the sidebar. I fixed this by updating the logout function to set User Chatromms ID to null on logout.l

--- src/containers/Messenger/Messenger.js
+++ src/containers/Messenger/Messenger.js
@@ -69,12 +69,14 @@ const Messenger = props => {
                 props.setAuthentication(false);
                 props.setUserId(null);
                 props.setUsername(null);
+                props.setUsersChatRoomsID(null);
             }
         } else { 
             /* Logout */
             props.setAuthentication(false);
             props.setUserId(null);
             props.setUsername(null);
+            props.setUsersChatRoomsID(null);
         }
     }
 
