0e982e8427ce09d140418df3b13011ece02a8d1d
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-12-01 13:12:24 -0500

    refractored updatePwdForm.js
    updated password validation. added confirm password field. broke large function into smaller function and changed input fields from type text to password

--- src/components/MainContent/Header/AccountSettings/AccountSettings.js
+++ src/components/MainContent/Header/AccountSettings/AccountSettings.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 import styles from "./AccountSettings.module.scss";
 import UpdatePwdForm from './UpdatePwdForm/UpdatePwdForm';
 import UpdateUsernameForm from './UpdateUsernameForm/UpdateUsernameForm';
@@ -6,15 +6,11 @@ import * as passwordHash from 'password-hash'; //import npm pass https://www.npm
 import DOMPurify from 'dompurify';
 import axios from '../../../../axios';
 
-/* 
-	TODO: Add pwd confirm field to updatePwdform and change inputs to type of pwd
-*/
-
 const AccountSettings = props => {
 	const [showUpdatePwd, setShowUpdatePwd] = useState(false);
 	const [showUpdateUsername, setShowUpdateUsername] = useState(false);
 
-	//checks if currrent password entered is equal to auth user pwd on db.
+	//checks if current password entered is equal to auth user pwd on db.
 	const checkPwd = async checkPassword => {
 		checkPassword = DOMPurify.sanitize(checkPassword);
 		checkPassword = checkPassword.replace(/[^\w^!?$]/g,'');
@@ -28,36 +24,38 @@ const AccountSettings = props => {
 		 }  
 	}
 
-	let body;
-	if(showUpdateUsername){	
-		body = (
-			<>
-				<button className={ styles.closeSettings } onClick={ () => props.setShowSettings(false) }>&times;</button>
-				<button className={styles.closeSettings}>larr;</button>
-				<h3 style={ { display: "inline" } }>Account settings</h3>
-				<UpdateUsernameForm checkPwd={ checkPwd } authUsername={ props.authUsername } setShowSettings={ props.setShowSettings } authUID={ props.authUID }/>
-			</>);
-	}else if(showUpdatePwd){
-		body = (
-			<>
-				<button className={ styles.closeSettings } onClick={ () => props.setShowSettings(false) }>&times;</button>
-				<h3 style={ { display: "inline" } }>Account settings</h3>
-				<UpdatePwdForm checkPwd={ checkPwd } setShowSettings={ props.setShowSettings } authUID={ props.authUID }/>
-			</>
-		);
-	}else{
-		body = (
-			<>
-				<button className={ styles.closeSettings } onClick={ () => props.setShowSettings(false) }>&times;</button>
-				<h3 style={ { display: "inline" } }>Account settings</h3>
-				<button className={styles.settingOptions} onClick={() => setShowUpdateUsername(true)}>Update Username</button>
-				<button className={styles.settingOptions} onClick={() => setShowUpdatePwd(true)}>Update Password</button>
-			</>
-		);
+	const goToAccountSettingsHome = () => {
+		setShowUpdateUsername(false);
+		setShowUpdatePwd(false);
 	}
+
+	const showBackBtn = () => {
+		if( showUpdateUsername || showUpdatePwd) {
+			return <button aria-label="Go back to account settings" onClick={goToAccountSettingsHome} className={styles.closeSettings}>&larr;</button>
+		}
+	}
+
+	const setAccountSettingsBody = () => {
+		if(showUpdateUsername) {	
+			return <UpdateUsernameForm checkPwd={ checkPwd } authUsername={ props.authUsername } setShowSettings={ props.setShowSettings } authUID={ props.authUID }/>;
+		} else if(showUpdatePwd) {
+			return <UpdatePwdForm checkPwd={ checkPwd } setShowSettings={ props.setShowSettings } authUID={ props.authUID }/>;		
+		} else {
+			return (
+				<>		
+					<button className={styles.settingOptions} onClick={ () => setShowUpdateUsername(true) }>Update Username</button>
+					<button className={styles.settingOptions} onClick={ () => setShowUpdatePwd(true) }>Update Password</button>
+				</>
+			);
+		}
+	}
+	
 	return(
 		<div className={ styles.container }> 
-			{body}
+			{showBackBtn()}
+			<button className={ styles.closeSettings } onClick={ () => props.setShowSettings(false) }>&times;</button>
+			<h3 style={ { display: "inline" } }>Account settings</h3>
+			{setAccountSettingsBody()}
 		</div>
 	);
 }
