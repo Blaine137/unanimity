9ad80dba057b7837c40af6dd4849105c9ced6fd8
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-12-02 16:00:56 -0500

    added emoji cart picker.
    You can now pick emoji and open the emoji cart. Started on logic to send emoji and am having trouble displaying emoji based on unicode version of the emoji.

--- src/components/MainContent/Input/Input.js
+++ src/components/MainContent/Input/Input.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Input.module.scss';
 import DOMPurify from 'dompurify';
+import Picker from 'emoji-picker-react';
 let oldTime = null;
 
-const input = props => {
+const Input = props => {
+    const [emojiPicker, setEmojiPicker] = useState(false);
+    let [userMessage, setUserMessage] = useState('');
+
+    /*
+        make aria label dynamic
+    */
+    const handleEmoji = (event, emojiObject) => {
+        setEmojiPicker(false);
+        props.newMessage(`<span role="img" aria-label="sheep">'\${emojiObject.originalUnified}'</span>` );
+    }
     return(
         <div className={ styles.inputContainer }>
+            { emojiPicker &&
+                <Picker onEmojiClick={ handleEmoji } />
+            }
             <textarea 
                 aria-label="Type a messages and press enter to send." 
                 spellCheck="true" 
                 placeholder="Press Enter to send Message" 
                 maxLength="1999"
                 className={ styles.input }
+                style={{display: emojiPicker ? 'none' : 'inline-block'} }
+                onChange={ e => {
+                    setUserMessage(DOMPurify.sanitize(e.target.value));
+                }}
                 onKeyDown={ e => {
-                    let userInput = DOMPurify.sanitize(e.target.value);         
                     if(e.key === 'Enter') {
                         if(props.currentChatRoomName && props.currentChatRoomName !== 'Unanimity') {
                             if( oldTime === null ) {                                      
@@ -23,7 +40,7 @@ const input = props => {
                             let currentTime = Date.now();                           
                             if(currentTime >= (oldTime + 2000)) {                               
                                     oldTime = currentTime;                                       
-                                    props.newMessage(userInput);
+                                    props.newMessage(userMessage);
                                     e.target.value = ''; //makes the input box empty once newMessage gets the input                            
                             } else {
                                 props.showAlert(" Please wait two seconds before sending another message! ");
@@ -32,11 +49,12 @@ const input = props => {
                             props.showAlert(' Please select a chatroom before sending a message! ');                           
                         }
                     }
-                } }             
+                }}             
             >
             </textarea>
+            <button onClick={ () => setEmojiPicker(true) }>Emoji</button>                  
         </div>
     );
 }
 
-export default input;
+export default Input;
