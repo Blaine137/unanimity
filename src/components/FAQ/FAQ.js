33db2f478ec602d1c57c674f581105738c10931b
transformerjnm <46799492+transformerjnm@users.noreply.github.com>
2020-12-03 13:04:37 -0500

    revised FQA page. Made the jsx more semantic and styled it for readablility

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -3,35 +3,27 @@ import Nav from '../Nav/Nav';
 import styles from './FAQ.module.scss';
 
 const FAQ = props => {
-
     return(
         <>
             <Nav/>
-            <img src="../../../unanimity-large-logo.svg" alt="Unanimity Messenger Logo. Harmony through words."/>
-            <h3>Frequently asked questions</h3>
-            <p className={styles.question}>how to add a chatroom?</p>
-            <ul className={styles.answer}>
-                <li>Once logged in, click on the green cross in the top left. Enter an existing user and chat away!</li>
-            </ul>
-            <p className={styles.question}>how to delete a chatroom?</p>
-            <ul className={styles.answer}>
-                <li>Login and click on the "X" next to the chatroom you would like to delete</li>
-            </ul>
-            <p className={styles.question}>how to send a message?</p>
-            <ul className={styles.answer}>
-                <li>Assuming you are logged in, click on a chatroom of your choice, then click in the white box on the bottom of the screen. Enter your message and hit enter!</li>
-            </ul>
-            <p className={styles.question}>how to send an emoji in a message?</p>
-            <ul className={styles.answer}>
-                <li>Simpily type out a colon ":" followed by the emoji you would like to use followed by another colon ":". <a href="https://github.com/tommoor/react-emoji-render/blob/HEAD/data/aliases.js" target="_blank">Here</a> is a link to all available emojis  </li>
-            </ul>
-            <p className={styles.question}>dont like our color sceme?</p>
-            <ul className={styles.answer}>
-                <li>You can change the theme to a dark theme by sliding the switch once you have logged in and clicked on the three dots in top right of your screen!</li>
-            </ul>
+            <main>
+                <img src="../../../unanimity-large-logo.svg" alt="Unanimity Messenger Logo. Harmony through words."/>
+                <h1>Frequently asked questions</h1>
+                <dl>
+                    <dt className={styles.question}>How do I add a chatroom?</dt>
+                    <dd>Once logged in, click on the green cross in the top left of the sidebar. Enter the username of who you want to chat with and chat away!</dd>
+                    <dt className={styles.question}>How do I delete a chatroom?</dt>
+                    <dd>Login and click on the "X" next to the chatroom you would click to delete.</dd>
+                    <dt className={styles.question}>How do I send a message?</dt>
+                    <dd>Once logged in, click on a chatroom of your choice, then click in the white box on the bottom of the screen. Type your message then press the enter key on the keyboard!</dd>
+                    <dt className={styles.question}>How do I send an emoji in a message?</dt>
+                    <dd>Simply type out a colon ":" followed by the emoji name you would like to use followed by another colon ":". Example ":smile:". <a href="https://github.com/tommoor/react-emoji-render/blob/HEAD/data/aliases.js" target="_blank" rel="noopener">View all emojis!</a></dd>
+                    <dt className={styles.question}>How do I change the color scheme?</dt>
+                    <dd>Once logged in, in the top right corner click on the three dots. This will open a option menu, where their will be a option titled theme. Click the switch beside theme to change the color scheme. </dd>
+                </dl>
+            </main>
         </>
     );
-
 };
 
 export default FAQ;
