9067475df0a2ead4fbb4a33f5798c98de917c0c9
Blaine Young <blaine.young3553@gmail.com>
2020-12-06 21:07:13 -0500

    fixed height and added animation on landing page

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -5,6 +5,33 @@ import { Link } from 'react-router-dom';
 import {motion} from 'framer-motion';
 
 const Landing = props => {
+
+    /* ANIMATION STYLES FOR FRAMER MOTION */
+		const pageVariants = {
+			initial: {
+			  opacity: 0,
+			  y: "-10vw",
+              scale: 0.8
+			},
+			in: {
+			  opacity: 1,
+			  y: 0,
+			  scale: 1
+			},
+			out: {
+			  opacity: 0,
+			  y: "100vw",
+			  scale: 1.2
+			}
+		  };
+		  
+		  const pageTransition = {
+			type: "tween",
+			ease: "anticipate",
+            duration: 0.5,
+            delay: 3.8
+		  };
+
     return (       
         <Fragment>
             <motion.div
@@ -17,7 +44,11 @@ const Landing = props => {
                 <main className={styles.backgroundImage}>
                     <img className={ styles.landinglogo } src="../../unanimity-large-logo.svg" alt="Unanimity Messenger Logo. Harmony through words."/>
                     <h1 className={ styles.header }>The best way to unify with your community!</h1>
-                    <p className={ styles.subtext }>Unanimity, instant communication that connects you to anyone.</p>          
+                    <motion.p className={ styles.subtext }
+                    initial="initial" 
+                    animate="in" 
+                    variants={pageVariants}
+                    transition={pageTransition}>Unanimity, instant communication that connects you to anyone.</motion.p>          
                     <button type="button" aria-label="Button that takes you to login page for Unanimity instant messenger."className={ styles.chatButton }>
                         <Link aria-label="Link that goes to login page" className={ styles.chatButton } to="/login">Start Chatting</Link>
                     </button>       
