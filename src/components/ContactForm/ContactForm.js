8e8839821bff7cf0508e9df154c4f248126371ce
Blaine Young <blaine.young3553@gmail.com>
2020-12-12 18:26:06 -0500

    added Emailjs to contact form

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -6,6 +6,8 @@ import { connect } from 'react-redux';
 import { setNotification } from '../../redux/actions';
 import Nav from '../Nav/Nav.js';
 import {motion} from 'framer-motion';
+import emailjs from 'emailjs-com';
+
 
 const mapStateToProps = state => ({ notification: state.messenger.notification });
 
@@ -15,7 +17,23 @@ const mapDispatchToProps = { setNotification };
 User form that takes in contact information, validates the email address, then alerts the user how to fix their data or that they will be receiving an email shortly. Their currently is no logic that sends the email.
 */
 let ContactForm = props => {
-    let closeNotification = () => props.setNotification(null);      
+
+    let closeNotification = () => props.setNotification(null);
+    
+    function sendEmail(e){
+        e.preventDefault();
+
+        emailjs.sendForm('contact_service', 'UnanimityContactTemplate', e.target, 'user_aaSkiLFIoRQuHKUSx1hvK')
+          .then((result) => {
+              console.log(result.text);
+              let message = "Thank you for contacting Unanimity, your form will be reviewed within 24 hours with emailjs!";
+              props.setNotification([<Alert alertMessage={ message } alertClose={ closeNotification }/>]);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset();
+    }
+
     return(     
         <main >
             <motion.div
@@ -27,31 +45,14 @@ let ContactForm = props => {
                 { props.notification }
                 <Nav />
                 <img src="../../../unanimity-large-logo.svg" alt="Unanimity Messenger Logo. Harmony through words."/>                         
-                <form className={ styles.form } action="/"> 
+                <form className={ styles.form } onSubmit={sendEmail}> 
                     <fieldset>
                         <legend>Let's get to know each other!</legend>                                 
                         <label htmlFor="Email" >Your Email Address</label>
-                        <input aria-label="Email Text input" type="Email" id="Email" name="Email" className={ styles.input } required/> 
+                        <input aria-label="Email Text input" type="Email" id="Email" name="Email" className={ styles.input } required /> 
                         <label htmlFor="Comments">How may we help you?</label>
                         <textarea aria-label="Enter your comment here for unanimity contact form." id="Comment" name="Comment" rows="12" placeholder="Enter your comment here." required></textarea>
-                        <input aria-label="Submit contact form to Unanimity" type="submit" value="Submit" className={ styles.submit } onClick={ e => {
-                                // sanitize and check for valid email format. then alert the user with appropriate info
-                                e.preventDefault();
-                                let email = document.getElementById('Email').value;
-                                let comment = document.getElementById('Comment').value;
-                                email = DOMPurify.sanitize(email) ;
-                                comment = DOMPurify.sanitize(comment);
-                                //only allow words numbers ! ? $ @ .                             
-                                email = email.replace(/[^\w^0-9^!?$@.]/g, '');
-                                comment = comment.replace(/[^\w^0-9^!?$@.]/g, '');
-                                //validate email format
-                                if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) && email && comment) {
-                                    let message = "Thank you for contacting Unanimity, your form will be reviewed within 24 hours!";
-                                    props.setNotification([<Alert alertMessage={ message } alertClose={ closeNotification }/>]);
-                                } else {
-                                    props.setNotification([<Alert alertMessage="Please enter a valid email address and comment" alertClose={ closeNotification }/>]);
-                                }                                       
-                        } }/> 
+                        <input aria-label="Submit contact form to Unanimity" type="submit" value="Submit" className={ styles.submit } /> 
                     </fieldset>                       
                 </form> 
             </motion.div>                     
