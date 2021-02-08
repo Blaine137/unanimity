5ce1c3390956b2eb9eb0160c9a540ea7c1216753
Blaine Young <blaine.young3553@gmail.com>
2021-02-08 16:24:30 -0500

    updated current chatroom ID prop #177

--- src/containers/Messenger/Messenger.js
+++ src/containers/Messenger/Messenger.js
@@ -470,7 +470,7 @@ const Messenger = (props) => {
         <div className={styles.sidebarGrid} style={sidebarInlineStyles}>
           <SidebarOfConversations
             authenticatedUsername={props.authenticatedUsername}
-            currentChatRoomID={props.currentChatRoomName}
+            currentChatRoomID={props.currentChatRoomID}
             usersChatRoomsID={sidebarUsersChatRoomsID}
             userID={props.authenticatedUserID}
             setCurrentChatRoomID={getCurrentChatRoom}
